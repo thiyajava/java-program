de9b0ca94692391b25b8d4ee5d377c191b53aa20
thiyajava <43058040+thiyajava@users.noreply.github.com>
2018-09-07 12:39:15 +0530

    Add files via upload

---
+++ mathcalculation.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e,f;
+    a=50;b=0.5f;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+     System.out.println("the addition of int and float output is"+c);
+     System.out.println("the subtraction of int and float output is"+d);    
+     System.out.println("the multiplication of int and float output is"+e);
+     System.out.println("the division of int and float output is"+f);
+  }
+}
